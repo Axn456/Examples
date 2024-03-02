02c7e92b1847f56a2320cecd7b4343b164a56a0a
evgdmitriev <evgdmitriev@ozon.ru>
2024-03-02 14:44:20 +0300

    final_project

---
+++ yandex_practicum_sql_data_analyst/advanced_sql/advanced_sql_final_project_part_01.sql
@@ -0,0 +1,150 @@
+﻿--Task_01
+select count (*)
+from stackoverflow.posts
+where post_type_id = 1
+ and (score > 300 or favorites_count >= 100)
+
+--Task_02
+select round(avg(c.cnt))
+from (select  date_trunc ('day', creation_date)::date dt
+      ,count(*) cnt
+	  from stackoverflow.posts
+	  where creation_date::date between '2008-11-01' and '2008-11-18'
+		  and post_type_id = 1
+	  group by dt
+	  order by dt)  c
+
+--Task_03
+select count (distinct u.id) users_id
+from stackoverflow.users u
+join stackoverflow.badges b on u.id=b.user_id
+where age(date_trunc ('day', u.creation_date::date), date_trunc ('day', b.creation_date::date)) = '0 days, 0:00:00'
+
+--Task_04
+with posti as (
+select *
+from stackoverflow.posts
+where user_id in (select id
+                    from stackoverflow.users
+                    where display_name like 'Joel Coehoorn'))
+
+select count(distinct p.id)
+from posti p
+join stackoverflow.votes v on p.id=v.post_id
+
+--Task_05
+select *
+      , rank () over (order by id desc) rnk
+from stackoverflow.vote_types
+order by rnk desc
+
+--Task_06
+with tmp as (
+select *
+from stackoverflow.votes
+where vote_type_id in (select id
+                       from stackoverflow.vote_types
+                       where name like 'Close'))
+
+select u.id us
+       , count(v.id) cnt
+from stackoverflow.users u
+join tmp t on u.id=t.user_id
+group by u.id
+order by cnt desc, us desc
+limit 10
+
+--Task_07
+with tmp as (
+select u.id
+, count (b.id) cnt
+from stackoverflow.users u
+join stackoverflow.badges b on u.id=b.user_id
+where b.creation_date::date between '2008-11-15' and '2008-12-15'
+group by u.id
+order by cnt desc, u.id
+limit 10)
+
+select *
+	,dense_rank() over (order by cnt desc)
+from tmp
+
+--Task_08
+select  title
+       ,user_id
+       ,score
+       ,round(avg(score) over (partition by user_id order by user_id))
+from stackoverflow.posts
+where title is not null
+and score != 0
+
+--Task_09
+select title
+from stackoverflow.posts
+where user_id in (select u.id
+                    from stackoverflow.users u
+                    join stackoverflow.badges b on u.id=b.user_id
+                    group by u.id
+                    having count (b.id) > 1000
+                  )
+and title is not null
+
+--Task_10
+select id
+       ,views
+       ,case
+           when views >= 350 then 1
+           when views between 100 and 349 then '2'
+           when views < 100 then '3'
+        end
+from stackoverflow.users
+where location like '%Canada%'
+and views > 0
+
+--Task_11
+with tmp as (
+select id
+   , cat
+   , views
+   , max(views) over (partition by cat) max
+ from (select id
+	   ,views
+	   ,case
+		   when views >= 350 then 1
+		   when views between 100 and 349 then '2'
+		   when views < 100 then '3'
+		end cat 
+		from stackoverflow.users
+		where location like '%Canada%'
+		and views > 0) t
+order by views desc, id)
+
+select id
+       ,cat
+       ,views
+from tmp
+where views - max = 0
+
+--Task_12
+select *
+      , sum(cnt) over (order by days)
+from (select extract(day from creation_date) days
+	  	  , count(id) cnt
+	  from stackoverflow.users
+	  where date_trunc('month', creation_date)::date = '2008-11-01'
+	  group by days) cnt
+
+--Task_13
+with tmp as (
+select t.id
+      , t.data_reg reg
+      , p.min_data min
+from (select id
+       , creation_date data_reg
+	  from stackoverflow.users) t
+join (select user_id id, min (creation_date) over (partition by user_id) min_data from stackoverflow.posts) p on t.id=p.id)
+
+select t.id
+     , t.min - reg razn
+from tmp t
+group by t.id, razn
---
+++ yandex_practicum_sql_data_analyst/advanced_sql/advanced_sql_final_project_part_02.sql
@@ -0,0 +1,72 @@
+﻿--Task_01
+select sum(views_count) sum_v
+     ,  date_trunc('month', creation_date)::date data_soz
+from stackoverflow.posts
+where extract(year from creation_date) = 2008
+group by data_soz
+having  sum(views_count) > 0
+order by sum_v desc
+
+--Task_02
+select u.display_name
+      , count(distinct t.user_id)
+from stackoverflow.users u
+join stackoverflow.posts t on u.id=t.user_id
+where t.post_type_id = 2
+and date_trunc('day', t.creation_date) <= date_trunc ('day', u.creation_date) + interval '1 month'
+group by u.display_name
+having count(t.id) > 100
+order by u.display_name
+
+--Task_03
+select count (id)
+     , date_trunc('month', creation_date)::date m
+from stackoverflow.posts 
+where user_id in (select p.user_id id_u
+                  from stackoverflow.posts p
+                  join stackoverflow.users u on p.user_id=u.id
+                  where date_trunc('month', p.creation_date)::date = '2008-12-01'
+                  and date_trunc('month', u.creation_date)::date = '2008-09-01')
+group by m
+order by m desc
+
+--Task_04
+select user_id
+       ,creation_date
+       ,views_count
+       ,sum(views_count) over (partition by user_id order by creation_date)
+from stackoverflow.posts
+
+--Task_05
+select round(avg(cnt))
+from (select user_id
+       ,count(distinct creation_date::date) cnt
+	  from stackoverflow.posts
+	  where creation_date::date between '2008-12-01' and '2008-12-07'
+	  group by user_id) u
+
+--Task_06
+with tmp as (
+select extract(month from creation_date) data
+, count(id) cnt
+from stackoverflow.posts
+where date_trunc('month', creation_date)::date between '2008-09-01' and '2008-12-01'
+group by data)
+
+
+select data
+	  , cnt
+      , round((cnt::numeric/lag(cnt) over (order by t.data)-1),4)*100
+from tmp t
+
+--Task_07
+with tmp as (
+select extract(week from creation_date) wk
+, max(creation_date) over (order by extract(week from creation_date)) max_dt
+from stackoverflow.posts post
+join (select user_id, count(id) cnt from stackoverflow.posts group by user_id order by cnt desc limit 1) t on post.user_id=t.user_id
+where date_trunc ('month', creation_date):: date = '2008-10-01')
+
+select *
+from tmp
+group by wk, max_dt
